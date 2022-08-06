2b1c06ac9f76994d67fbca7b93640b2fe5de5011
Maxime Laroche <mlaroche2009@gmail.com>
2022-08-06 09:03:02 -0400

    Made website depend on external JSON Will no longer need to make deployments to edit data

--- src/app/services/http/data-api.service.ts
+++ src/app/services/http/data-api.service.ts
@@ -1,39 +1,47 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
-
+const SERVER_URL =
+  'https://raw.githubusercontent.com/MaximeLaroche/Recherche-Stage/BaseAnglais/';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataApiService {
-
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
   getProfile(): Promise<any> {
-    return this.http.get('assets/profile.json').toPromise();
+    return this.http.get(SERVER_URL + 'assets/profile.json').toPromise();
   }
 
   getTopSkills(): Promise<any> {
-    return this.http.get('assets/top-skills.json').toPromise();
+    return this.http.get(SERVER_URL + 'assets/top-skills.json').toPromise();
   }
 
   getFeaturedProjects(): Promise<any> {
-    return this.http.get('assets/projects.json').toPromise();
+    return this.http.get(SERVER_URL + 'assets/projects.json').toPromise();
   }
 
   getWorkHistory(): Promise<any> {
-    return this.http.get('assets/work-experience.json').toPromise();
+    return this.http
+      .get(SERVER_URL + 'assets/work-experience.json')
+      .toPromise();
   }
 
   getCV(): Promise<any> {
     let headers = new HttpHeaders();
-    headers.append('Access-Control-Allow-Headers', 'Content-Type,application/json');
+    headers.append(
+      'Access-Control-Allow-Headers',
+      'Content-Type,application/json'
+    );
     headers.append('Access-Control-Allow-Methods', 'GET');
     headers.append('Access-Control-Allow-Origin', 'http://localhost:4200');
 
-    return this.http.get('https://github.com/MaximeLaroche/Recherche-Stage/blob/BaseAnglais/CV.pdf', { headers: headers }).toPromise();
+    return this.http
+      .get(
+        'https://github.com/MaximeLaroche/Recherche-Stage/blob/BaseAnglais/CV.pdf',
+        { headers: headers }
+      )
+      .toPromise();
   }
 }
